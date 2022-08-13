7923d732ecdf44b8671dff18e938b7a0ed2378aa
JR132662 <cooljon08@gmail.com>
2022-08-13 15:28:33 +0000

    save

--- src/models.py
+++ src/models.py
@@ -15,5 +15,49 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
+        }
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    haircolor = Column(String)
+    eyecolor = Column(String)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "haircolor": self.haircolor,
+            "eyecolor": self.eyecolor,
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    climate = Column(String)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    character = relationship(Character)
+    character_id = Column(Integer, ForeignKey('character.id'),nullable=True)
+    planet = relationship(Planet)
+    planet_id = Column(Integer, ForeignKey('planet.id'),nullable=True)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    def serialize(self):
+        return {
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            "user_id": self.user_id,
         }
